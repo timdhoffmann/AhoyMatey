f6c57f9e5cb9496faf14ccff7f32b9baf97c2816
Tim <9947384+timdhoffmann@users.noreply.github.com>
2019-10-19 13:35:14 +0200

    Added project files without LFS.

---
+++ Assets/_Project/Managers/MyNetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    public void StartAsHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + ": Starting Host.");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + ": Host started.");
+    }
+
+    public override void OnStartClient (NetworkClient client)
+    {
+        base.OnStartClient(client);
+        Debug.Log(Time.timeSinceLevelLoad + ": Client started.");
+        InvokeRepeating("IndicateConnecting", 0f, 1f);
+    }
+
+    public override void OnClientConnect (NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log(Time.timeSinceLevelLoad + ": Client connected to address " + conn.address);
+        CancelInvoke();
+    }
+
+    private void IndicateConnecting()
+    {
+        Debug.Log("...connecting.");
+    }
+}
---
+++ Assets/_Project/Player/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerControl : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    private Vector3 movementInput;
+    private Camera localCam;
+
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        movementInput.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        movementInput.y = 0f;
+        movementInput.z = CrossPlatformInputManager.GetAxis("Vertical");
+        transform.Translate(movementInput);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        // Enable only the local player's camera component (disabled in prefab).
+        // The camera gameObject has to be enabled to find the component.
+        localCam = GetComponentInChildren<Camera>();
+        localCam.enabled = true;
+    }
+}
